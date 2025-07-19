9cb2fbb8f9ec472326c87347b6e43ac9bd73d4bc
kumneger Getiye <159164899+assumaqn@users.noreply.github.com>
2025-07-19 13:09:24 +0000

    feat: render Card component on landing page and set up proper export

--- alx-project-0x00/components/Pill.tsx
+++ alx-project-0x00/components/Pill.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-
 const Pill: React.FC<{ title?: string }> = ({ title = "Title" }) => {
   return (
     <div className="flex justify-center items-center bg-[#F9F9F9] px-3 w-auto h-[27px] rounded-full">
