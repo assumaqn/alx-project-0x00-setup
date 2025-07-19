49382be9757effcafc03e0625a67393ef1c21f5e
kumneger Getiye <159164899+assumaqn@users.noreply.github.com>
2025-07-19 13:15:48 +0000

    feat: type Pill component with props using TypeScript interface

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,10 +1,12 @@
+// pages/landing.tsx
 import React from 'react';
 import Card from "@/components/Card";
 
 const Landing: React.FC = () => {
   return (
-    <div>
+    <div className="p-6">
       <h1 className="text-xl font-extralight">Landing Page</h1>
+      <Card />
       <Card />
     </div>
   );
